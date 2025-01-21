06808421f0a453d1d6017781bcdf816c189fe175
svchaudhari <swapchaudhari60@gmail.com>
2025-01-21 14:04:31 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -8,10 +8,10 @@ fi
 
 if [ -f "${FOLDER}pom.xml" ]; then
     # Extract the version using grep and sed
-    grep -oPm1 "(?<=<version>)[^<]+" "${FOLDER}pom.xml" | cut -d'-' -f1
+    grep -oPm2 "(?<=<version>)[^<]+" "${FOLDER}pom.xml" | tail -n 1 | cut -d'-' -f1
 elif [ -f "${FOLDER}package.json" ]; then
-    # Extract the version using grep and sed
-    grep -oPm1 '(?<=\b"version": ")[^"]+' "${FOLDER}package.json"
+    VERSION=$(sed -n 's/.*"version": "\(.*\)",/\1/p' ${FOLDER}package.json)
+    echo $VERSION
 elif [ -f "${FOLDER}VERSION" ]; then
     # Simply output the content of the VERSION file
     cat "${FOLDER}VERSION"
