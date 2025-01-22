6fcdb5ed387d54ca95c34e5163af7e2a598b57d8
svchaudhari <swapchaudhari60@gmail.com>
2025-01-22 14:24:17 +0530

    Update deployment-admin.sh

--- deployment-admin.sh
+++ deployment-admin.sh
@@ -24,7 +24,7 @@ CPU_REQUEST="250m"
 MEMORY_LIMIT="512Mi"
 CPU_LIMIT="500m"
 DEPLOY_DB_VARS=true
-EXTERNAL_ENV_FILE="/home/smartpds/k8s-smartpds/admin/external-var-admin.txt"  # Path to external file
+EXTERNAL_ENV_FILE="external-var-admin.txt"  # Path to external file
 
 
 
